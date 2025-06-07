0cb4a814232356f3761a9245d55c5f946992e4f7
Gabriel <gfernandes10@hotmail.com.br>
2025-06-07 17:36:34 -0300

    Remove source_workspace.sh script as it is no longer needed

--- source_workspace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Source ROS2 Foxy and the workspace, set Gazebo model path, and source Gazebo environment
-
-source /opt/ros/foxy/setup.bash
-source install/setup.bash
-export GAZEBO_MODEL_PATH=${PWD}/install/tello_gazebo/share/tello_gazebo/models
-source /usr/share/gazebo/setup.sh
-
-echo "Environment ready! You can now run Gazebo and ROS2 launch files."
